5fe047de9430475339bc0e9ac534380ccac699ff
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-16 12:52:02 -0500

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -6,6 +6,9 @@ public class Intro{
   private backStory() {
     System.out.println("You awake to the sound of water droplets pattering around you.");
     thread.sleep(2000);
-    System.out.println("A breeze of cold air brings you to. You open your eyes only to find yourself completely enshrouded in darkness");
+    System.out.println("A breeze of cold air brings you to. You open your eyes only to find yourself completely surrounded by darkness");
+    System.out.println("As you come to, you begin to remember how you got here.");
+    System.out.println("Wasn't I...");
+    thread.sleep(750);
   }
 } 
