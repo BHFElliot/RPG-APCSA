cb20e9a6863c36fd304c48a8c22e2c2eed0a4e96
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-16 12:20:07 -0500

    Create Intro.java

---
+++ Intro.java
@@ -0,0 +1,11 @@
+public class Intro{
+  public Intro(){
+    backStory();  
+  }
+  //System.out.println(""); 
+  private backStory() {
+    System.out.println("You awake to the sound of water droplets pattering around you.");
+    thread.sleep(2000);
+    System.out.println("A breeze of cold air brings you to. You open your eyes only to find yourself completely enshrouded in darkness");
+  }
+} 
