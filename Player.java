cec53563ba4633564a13978131043df763978c11
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-16 17:21:10 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,5 @@
+package game;
+
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
@@ -12,6 +14,12 @@ public class Player {
 		create();
 	}
 	
+	public Player(String a, char b) {
+		name = a;
+		gender = b;
+		confirm();
+	}
+	//below are the getters and setters for gender and name. These can only be invoked by special events.
 	public char getGender() {
 		return gender;
 	}
@@ -28,16 +36,20 @@ public class Player {
 		name = newName;
 	}
 	
+	//this monster of a method is basically a constructor
 	private void create() {
 		name = JOptionPane.showInputDialog("Input a character name please");
-		String genders[] = {"Male", "Female", "Other"};
+		String genders[] = {"Male", "Female", "Other"}; //these are the options for the gender selecting window.
+		
 		while(name == null ||name.equals("")) {
 			System.out.println("You can't name your character nothing!");
 			name = JOptionPane.showInputDialog("Input a character name please");
 		}
 		
-		int characterGender = JOptionPane.showOptionDialog (null, "Input the character's gender please.", "Character Creator", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, genders, 0);
+		int characterGender = Main.optionDialog("Input the character's gender please.", "Character Creator", 2, genders);
+				//JOptionPane.showOptionDialog (null, "Input the character's gender please.", "Character Creator", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, genders, 0);
 		
+		//the non null parameters are (in order): The message in the window, the window title, how many buttons there are, the symbol next to the question, and the words on the buttons.
 		switch(characterGender) {
 			case 0: gender = 'M';
 			break;
@@ -46,9 +58,15 @@ public class Player {
 			case 2: gender = 'O';
 			break;
 		}
+		confirm();
 		
-		if (JOptionPane.showOptionDialog (null, "You've selected:\n" + name + " with gender: " + gender, "Character Creator", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, 0) == 0)
+	}
+	/*this is basically the same thing as the last window, although the main difference is that selecting no starts the whole process over again. 
+	Forcing the player to hard reset if they want to change their character's name and gender is really annoying for the user.*/
+	private void confirm() {
+		if (Main.optionDialog("You've selected: " + name + " with gender: " + gender + "\nIs this ok?", "confirmation", 2, null) == 0)
 			System.out.println("good luck!");
 		else
 			create();
 	}
+}
