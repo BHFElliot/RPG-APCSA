25792ea97fd926cacda07e12ce7998ac1606944c
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-16 17:20:40 -0500

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -1,14 +1,18 @@
+package game;
+
 public class Intro{
-  public Intro(){
+  public Intro() {
     backStory();  
   }
-  //System.out.println(""); 
-  private backStory() {
-    System.out.println("You awake to the sound of water droplets pattering around you.");
-    thread.sleep(2000);
-    System.out.println("A breeze of cold air brings you to. You open your eyes only to find yourself completely surrounded by darkness");
-    System.out.println("As you come to, you begin to remember how you got here.");
+  private void backStory() {
+	Main.clearConsole();
+    Main.printSleep(1000, "You awake to the sound of water droplets pattering around you.");
+    Main.printSleep(2000, "A breeze of cold air brings you to. You open your eyes only to find yourself completely enshrouded in darkness.");
+    Main.printSleep(1250,"As you come to, you begin to remember how you got here.");
     System.out.println("Wasn't I...");
-    thread.sleep(750);
+  }
+  
+  private static void awakenChoice() {
+	  
   }
 } 
