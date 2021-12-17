d05f13316f1ff6d9c6f753d4491db3aa8c1891d9
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-17 08:57:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package game;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
-/* This kickstarts the game.
+/* This class kickstarts the game.
  * 
  * @Author: Elliot Miller
  * @Version: 1.0
@@ -19,12 +19,12 @@ public class Main {
 	public static void main(String[] args) {
 		player = characterCreate();
 		playerSheet = new ResourceSheet(player);	
-		new Intro();
+		Intro.backStory();
 	}
 	
 	//asks the player if they want to make a custom character.
 	public static Player characterCreate() {
-		switch (optionDialog("Hi! Thank you for playing spelunk. \nWhen presented with a choice, a window will pop up allowing you to choose what you'd like to do."
+		switch (Printers.optionDialog("Hi! Thank you for playing spelunk. \nWhen presented with a choice, a window will pop up allowing you to choose what you'd like to do."
 				+ "\nWould you like to create a character? Default values will be assigned to your character if not.", "Welcome!", 2, null)) {
 		case 0: return new Player();
 		case 1: return new Player("Explorer", 'M');
@@ -32,42 +32,6 @@ public class Main {
 		return null;
 	}
 	
-	//these methods are just me not wanting to look at 1000 try catch statements every time I use Thread.sleep().
-	public static void sleep(int a) {
-	    try {
-			Thread.sleep(a);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void printSleep(int a, String b) {
-		System.out.println(b);
-		try {
-			Thread.sleep(a);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	//prints a bunch of times lol
-	public static void clearConsole() {
-		for(int i = 0; i < 10; i++) {
-			System.out.println();
-		}
-	}
 
-	//creates an option window.
-	public static int optionDialog(String bodyText, String title, int option, String[]responses) {
-		
-		if(option == 1)	
-			return JOptionPane.showOptionDialog(null, bodyText, title, JOptionPane.YES_NO_CANCEL_OPTION, 
-			JOptionPane.PLAIN_MESSAGE, null, responses, 0);
-		else if(option == 2)
-			return JOptionPane.showOptionDialog(null, bodyText, title, JOptionPane.YES_NO_OPTION, 
-			JOptionPane.PLAIN_MESSAGE, null, responses, 0);
-		else 
-			return JOptionPane.showOptionDialog(null, bodyText, title, JOptionPane.YES_OPTION, 
-			JOptionPane.PLAIN_MESSAGE, null, responses, 0);
-	}	
 	
 }
