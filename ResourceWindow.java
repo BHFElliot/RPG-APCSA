60ab269d57b5b4819e0acc352102bda38f9a17a5
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-17 12:07:13 -0500

    Update ResourceWindow.java

--- ResourceWindow.java
+++ ResourceWindow.java
@@ -1,10 +1,13 @@
 import javax.Swing.JFrame;
+import javax.Swing.JLabel;
 
 public class ResourceWindow extends JFrame{
   public ResourceWindow() {
     this.setSize(500,500);
     this.setResizable(false);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    JLabel sanity = new JLabel("Sanity");
     
     this.setVisible(true);
   }
