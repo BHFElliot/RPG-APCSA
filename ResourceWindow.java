d6da02302e464d45e66888163033ab60a574c96b
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-17 12:05:08 -0500

    Create ResourceWindow.java

---
+++ ResourceWindow.java
@@ -0,0 +1,11 @@
+import javax.Swing.JFrame;
+
+public class ResourceWindow extends JFrame{
+  public ResourceWindow() {
+    this.setSize(500,500);
+    this.setResizable(false);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    this.setVisible(true);
+  }
+}
