52f2252a2262a74c02cd7acc106e64ca4b1cb695
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-19 09:17:41 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package game;
 
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
+
 /* This class kickstarts the game.
  * 
  * @Author: Elliot Miller
@@ -19,17 +19,29 @@ public class Main {
 	public static void main(String[] args) {
 		player = characterCreate();
 		playerSheet = new ResourceSheet(player);	
+		new ResourceDisplay();
 		Intro.backStory();
 	}
 	
 	//asks the player if they want to make a custom character.
 	public static Player characterCreate() {
-		switch (Printers.optionDialog("Hi! Thank you for playing spelunk. \nWhen presented with a choice, a window will pop up allowing you to choose what you'd like to do."
-				+ "\nWould you like to create a character? Default values will be assigned to your character if not.", "Welcome!", 2, null)) {
+		Printers.messageWindow("Welcome to spelunk! This is a text-based choose your own adventure style game."
+				+ "\nThe game's mechanics are very simple. Whenever you're presented with a choice, A window will pop up showing your options."
+				+ "\nA resource window will also appear displaying your character's sanity, health, name, and gender.", null);
+		
+		if(Printers.optionDialog("Would you like an explanation of the sanity system?", null, 2, null) == 0) 
+			sanityExplanation();
+		
+		switch (Printers.optionDialog("Would you like to create a character? Default values will be assigned to your character if not.", null, 2, null)) {
 		case 0: return new Player();
 		case 1: return new Player("Explorer", 'M');
 		}
 		return null;
+	}
+
+	public static void sanityExplanation() {
+		Printers.messageWindow("Certain events and decisions can raise or lower your sanity. Certain events also require a sanity check to pass,"
+				+ "\n meaning that if your sanity is too low you're locked out of a decision that a more mentally sound person would make.", null);
 	}
 	
 
