eae80808bb422105f0273710344a48b64fb40846
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-20 17:38:20 -0500

    Update Printers.java

--- Printers.java
+++ Printers.java
@@ -17,8 +17,17 @@ public class Printers {
 		}
 	}
 	
-	public static void printSleep(int sleepTimeMS, String text) {
+	public static void printlnSleep(int sleepTimeMS, String text) {
 		System.out.println(text);
+		try {
+			Thread.sleep(sleepTimeMS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printSleep(int sleepTimeMS, String text) {
+		System.out.print(text);
 		try {
 			Thread.sleep(sleepTimeMS);
 		} catch (InterruptedException e) {
@@ -45,8 +54,55 @@ public class Printers {
 			return JOptionPane.showOptionDialog(null, bodyText, title, JOptionPane.YES_OPTION, 
 			JOptionPane.PLAIN_MESSAGE, null, responses, 0);
 	}	
-	
+	//creates an option window with no title
+	public static int optionDialog(String bodyText, int options, String[]responses) {
+		
+		if(options == 1)	
+			return JOptionPane.showOptionDialog(null, bodyText, null, JOptionPane.YES_NO_CANCEL_OPTION, 
+			JOptionPane.PLAIN_MESSAGE, null, responses, 0);
+		else if(options == 2)
+			return JOptionPane.showOptionDialog(null, bodyText, null, JOptionPane.YES_NO_OPTION, 
+			JOptionPane.PLAIN_MESSAGE, null, responses, 0);
+		else 
+			return JOptionPane.showOptionDialog(null, bodyText, null, JOptionPane.YES_OPTION, 
+			JOptionPane.PLAIN_MESSAGE, null, responses, 0);
+	}	
+	//creates a message window (holds information and has only one option)
 	public static void messageWindow(String bodyText, String title) {
 		JOptionPane.showMessageDialog(null, bodyText, title, JOptionPane.PLAIN_MESSAGE);
+	}
+	
+	public static void printAct(int a) {
+		switch(a) {
+		case 0:
+			System.out.println("----------------------------------------------");
+			System.out.println("  ____            _                        \n"
+					+ " |  _ \\ _ __ ___ | | ___   __ _ _   _  ___ \n"
+					+ " | |_) | '__/ _ \\| |/ _ \\ / _` | | | |/ _ \\\n"
+					+ " |  __/| | | (_) | | (_) | (_| | |_| |  __/\n"
+					+ " |_|   |_|  \\___/|_|\\___/ \\__, |\\__,_|\\___|\n"
+					+ "                          |___/            ");
+			System.out.println("----------------------------------------------\n");
+
+			break;
+		case 1:
+			System.out.println("----------------------------------------------");
+			System.out.println("     _        _      ___              \n"
+					+ "    / \\   ___| |_   / _ \\ _ __   ___  \n"
+					+ "   / _ \\ / __| __| | | | | '_ \\ / _ \\ \n"
+					+ "  / ___ \\ (__| |_  | |_| | | | |  __/ \n"
+					+ " /_/   \\_\\___|\\__|  \\___/|_| |_|\\___|\n");
+			System.out.println("----------------------------------------------\n");
+			break;
+		case 2:
+			System.out.println("----------------------------------------------");
+			System.out.println("     _        _     _____               \n"
+					+ "    / \\   ___| |_  |_   _|_      _____  \n"
+					+ "   / _ \\ / __| __|   | | \\ \\ /\\ / / _ \\ \n"
+					+ "  / ___ \\ (__| |_    | |  \\ V  V / (_) |\n"
+					+ " /_/   \\_\\___|\\__|   |_|   \\_/\\_/ \\___/ ");
+			System.out.println("----------------------------------------------\n");
+		}
+		
 	}
 }
