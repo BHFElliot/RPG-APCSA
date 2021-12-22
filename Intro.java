847ed4ea2e579db6d3daaaaa58c9aa20e294f4a8
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-22 09:13:26 -0700

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -14,14 +14,40 @@ public class Intro{
 	System.out.println("\n");
     Printers.printAct(0);
 	Printers.printlnSleep(2000, "You awake to the sound of water droplets pattering around you.");
-    Printers.printlnSleep(3500, "A breeze of cold air brings you to. You open your eyes only to find yourself completely enshrouded in darkness.");
+    Printers.printlnSleep(3500, "A breeze of cold air wakes you up. You open your eyes only to find yourself completely enshrouded in darkness.");
     Printers.printlnSleep(2250,"As you come to, you begin to remember how you got here.");
 
     awakenChoice();
   }
+  //all of the players choices.
+  private static void awakenChoice() {
+	  switch(Printers.optionDialog("Wasn't I...", null, 1, new String[]{"Going spleunking?","Taking a nap alone in a dark cave?"})) {
+	  	case 0: introPartOne(true); break;
+	  	case 1: Main.playerSheet.addSanity(-10); introPartOne(false); break;
+	  }
+  }
   
-  //this is the backstory and allows the player to make a VERY important decision about what path they want to go down.
-  private static void introPartOne(boolean a) {
+  private static void flashlightChoice() {
+	  switch(Printers.optionDialog("Should I turn my flashlight on?", 1, new String[]{"Of course! I can't get out of here without any light.","No way. Who knows what could be lurking down here?"})) {
+	  	case 0: 
+	  		Printers.printlnSleep(2000, "You go to turn on your phone, and...");
+	  		//THIS HAS BEEN ALTERED, IT SHOULD BE 1 NOT 77
+	  		if (new Random().nextInt(2) == 77) {
+	  			Printers.printlnSleep(1000, "It's dead.");
+	  			darkness();	
+	  		}	
+	  		else {
+	  			CivPath.begin();
+	  		}	
+	  		break;
+	  	case 1: 
+	  		Main.playerSheet.addSanity(-5); 
+	  		CivPath.begin();
+	  }
+  }
+  
+  //this is the backstory and allows the player to make a decision about what path they want to go down.
+  public static void introPartOne(boolean a) {
 	  if(a) {
 		  Printers.printlnSleep(2000, "You begin to remember what happened before you woke up.");
 		  Printers.printlnSleep(2500, "\"Of course! I must've slipped and fell when I was checking out that cave.\"");
@@ -40,7 +66,7 @@ public class Intro{
 	 
   }
   
-  private static void darkness() {
+  public static void darkness() {
 	  Printers.printlnSleep(3000, "You've seen enough horror movies to know no good can come from being alone in caves. This is probably for the best.");
 	  Printers.printlnSleep(3000, "You stumble around the cave, feeling around for any sort of exit and trying your best to avoid any additional falls.");
 	  Printers.printlnSleep(1500, "Suddenly, your finger strikes something...");
@@ -50,38 +76,6 @@ public class Intro{
   
   //Printers.printSleep(2000,""); this is what I'm using for copy and pasting. Hopefully it's been removed by the time I turn this in.
   
-  //all of the players choices.
-  private static void awakenChoice() {
-	  switch(Printers.optionDialog("Wasn't I...", null, 1, new String[]{"Going spleunking?","Taking a nap alone in a dark cave?"})) {
-	  	case 0: introPartOne(true); break;
-	  	case 1: Main.playerSheet.addSanity(-10); introPartOne(false); break;
-	  }
-  }
-  
-  private static void flashlightChoice() {
-	  switch(Printers.optionDialog("Should I turn my flashlight on?", 1, new String[]{"Of course! I can't get out of here without any light.","No way. Who knows what could be lurking down here?"})) {
-	  	case 0: 
-	  		Printers.printlnSleep(2000, "You go to turn on your phone, and...");
-	  		if (new Random().nextInt(2) == 1) {
-	  			Printers.printlnSleep(1000, "It's dead.");
-	  			darkness();
-	  		}	
-	  		else {
-	  			lightEvents();
-	  		}	
-	  		break;
-	  	case 1: 
-	  		Main.playerSheet.addSanity(-5); 
-	  		lightEvents();
-	  }
-  }
-  
-  //just picks if the player goes down the civilization path or the organism path, commented for now to allow development on the civ path.
-  private static void lightEvents() {
-	  CivPath.begin();
-	  /*if (new Random().nextInt(2) == 1)
-		  CivPath.begin();
-	  else
-		  BioPath.begin();*/
-  }
+
+
 } 
