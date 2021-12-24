45728b64d321b362a8d65d91819f298c4d217961
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-23 19:13:44 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,3 @@
-package game;
-
-import java.util.Scanner;
 
 
 /* This class kickstarts the game.
@@ -21,19 +18,19 @@ public class Main {
 		playerSheet = new ResourceSheet(player);	
 		new ResourceDisplay();
 		Intro.backStory();
-		//CivPath.begin();
+		//Settlement.begin();
 	}
 	
 	//asks the player if they want to make a custom character.
 	public static Player characterCreate() {
 		Printers.messageWindow("Welcome to spelunk! This is a text-based choose your own adventure style game."
-				+ "\nThe game's mechanics are very simple. Whenever you're presented with a choice, A window will pop up showing your options."
+				+ "\nThe game's mechanics are very simple. Whenever you're presented with a choice, a window will pop up showing your options."
 				+ "\nA resource window will also appear displaying your character's sanity, health, name, and gender.", null);
 		
-		if(Printers.optionDialog("Would you like an explanation of the sanity system?", null, 2, null) == 0) 
+		if(Printers.optionDialog("Would you like an explanation\nof the sanity system?", null, 2, null) == 0) 
 			sanityExplanation();
 		
-		switch (Printers.optionDialog("Would you like to create a character? Default values will be assigned to your character if not.", null, 2, null)) {
+		switch (Printers.optionDialog("Would you like to create a character?\nDefault values will be assigned\nto your character if not.", null, 2, null)) {
 		case 0: return new Player();
 		case 1: return new Player("Explorer", 'M');
 		}
@@ -41,10 +38,7 @@ public class Main {
 	}
 
 	public static void sanityExplanation() {
-		Printers.messageWindow("Certain events and decisions can raise or lower your sanity. Certain events also require a sanity check to pass,"
+		Printers.messageWindow("Certain events and decisions can raise or lower your sanity.\nCertain events also require a sanity check to pass,"
 				+ "\n meaning that if your sanity is too low you're locked out of a decision that a more mentally sound person would make.", null);
 	}
-	
-
-	
 }
