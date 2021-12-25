ec7f5f53cd737c34324ac3e9b302bcb655fbc89e
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2021-12-25 09:54:15 -0700

    Update and rename Intro.java to Start.java

--- Start.java
+++ Start.java
@@ -1,18 +1,15 @@
 package game;
 
-import java.util.Random;
-
-
-public class Intro{
-  /* This class contains the game's intro, which is the main branching point for the different paths.
+public class Start{
+ /**
+  *  The Start class contains the game's intro.
   * 
   * @Author: Elliot Miller
   * @Version: 1.0
-  * @Since: 12-16-21
+  * @Since: 2021-12-16
   */ 
   public static void backStory() {
 	System.out.println("\n");
-    Printers.printAct(0);
 	Printers.printlnSleep(2000, "You awake to the sound of water droplets pattering around you.");
     Printers.printlnSleep(3500, "A breeze of cold air wakes you up. You open your eyes only to find yourself completely enshrouded in darkness.");
     Printers.printlnSleep(2250,"As you come to, you begin to remember how you got here.");
@@ -26,8 +23,8 @@ public class Intro{
 	  	case 1: Main.playerSheet.addSanity(-10); introPartOne(false); break;
 	  }
   }
-  
-  private static void flashlightChoice() {
+  //these methods aren't in use anymore but they took a while to write so imma keep them just in case.
+  /*private static void flashlightChoice() {
 	  switch(Printers.optionDialog("Should I turn my flashlight on?", 1, new String[]{"Of course! I can't get out of here without any light.","No way. Who knows what could be lurking down here?"})) {
 	  	case 0: 
 	  		Printers.printlnSleep(2000, "You go to turn on your phone, and...");
@@ -37,29 +34,12 @@ public class Intro{
 	  			darkness();	
 	  		}	
 	  		else {
-	  			CivPath.begin();
+	  			House.begin();
 	  		}	
 	  		break;
 	  	case 1: 
 	  		Main.playerSheet.addSanity(-5); 
-	  		CivPath.begin();
-	  }
-  }
-  
-  //this is the backstory and allows the player to make a decision about what path they want to go down.
-  public static void introPartOne(boolean a) {
-	  if(a) {
-		  Printers.printlnSleep(2000, "You begin to remember what happened before you woke up.");
-		  Printers.printlnSleep(2500, "\"Of course! I must've slipped and fell when I was checking out that cave.\"");
-		  Printers.printlnSleep(4000, "Thankfully, you still have your phone on you.");
-		  Printers.printlnSleep(3000, "You reach into your bag and pull out your phone. "
-		  							+ "Do you want to turn the flashlight on?");
-		  flashlightChoice();
-	  } else {
-		  Printers.printlnSleep(2000, "You begin to remember what happened before you woke up.");
-		  Printers.printlnSleep(4000, "\"Of course! I must've been out for one of my solo cave walks when I got tired and took a nap in this crevace.\"");
-		  Printers.printlnSleep(3000, "You feel around your pockets and find your phone. Do you try and use the flashlight feature to illuminate your surroundings?");
-		  flashlightChoice();
+	  		House.begin();
 	  }
   }
   
@@ -67,6 +47,23 @@ public class Intro{
 	  Printers.printlnSleep(3000, "You've seen enough horror movies to know no good can come from being alone in caves. This is probably for the best.");
 	  Printers.printlnSleep(3000, "You stumble around the cave, feeling around for any sort of exit and trying your best to avoid any additional falls.");
 	  Printers.printlnSleep(1500, "Suddenly, your finger strikes something...");  
+  }*/
+
+	 
+  
+  //this is the backstory and allows the player to make a decision about what path they want to go down.
+  public static void introPartOne(boolean a) {
+	  if(a) {
+		  Printers.printlnSleep(2000, "You begin to remember what happened before you woke up.");
+		  Printers.printlnSleep(2500, "\"Of course! I must've slipped and fell when I was checking out that cave.\"");
+		  Printers.printlnSleep(4000, "Thankfully, you still have your phone on you.");
+	  } else {
+		  Printers.printlnSleep(2000, "You begin to remember what happened before you woke up.");
+		  Printers.printlnSleep(4000, "\"Of course! I must've been out for one of my solo cave walks when I got tired and took a nap in this crevace.\"");
+		  Printers.printlnSleep(3000, "You feel around your pockets and find your phone.");
+	  }
+	  House.begin();
   }
 
-} 
+}
+
