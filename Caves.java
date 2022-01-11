934ed41df914d00bbd3cdb8edcfeecdd82bdad0c
BHFElliot <96197365+BHFElliot@users.noreply.github.com>
2022-01-11 15:43:33 -0500

    Update Caves.java

--- Caves.java
+++ Caves.java
@@ -1,135 +1,92 @@
 package game;
 
-public class Settlement {
-	
+public class Caves {
 	/**
-	 *The Settlment class is gameplay and dialogue in the town.
+	 * The Caves class contains what happens to the player when they leave the town.
 	 * 
-	 *@Author: Elliot Miller
-	 *@Since: 2021/12/22
-	 *@Version: 1.0
-	 *
+	 * @author Elliot Miller
+	 * @version 1.0
+	 * @since 2021-12-21
 	 */
-	
-	public static boolean isHostile = Roll.silentRoll(1,2);
-	public static boolean hasBeen = false;
 	public static void begin() {
-		hasBeen = true;
-		Printers.printlnSleep(3000,"After what felt like an eternity of walking, you finally "
-				+ "reach the end of the cramped tunnel you've been travelling through.");
-		Printers.printlnSleep(2000, "On the other side, you spot a huge walled off area.");
-		Printers.printlnSleep(3000, "There's an entrance with a guard sitting right beside it, reading a book.");
-		Printers.printlnSleep(2000, "You approach the guard and ask how you can get into the town.");
-		Printers.printlnSleep(1000, "The guard responds: \"Do you have a passport?\"");
-		Printers.printlnSleep(2000, "You tell the guard you might have an id, and reach for your wallet.");
-		if(Roll.silentRoll(1, 3))
-			id();
-		else
-			noID();
+		Main.playerSheet.setPlayerPosition('C');
+		Printers.printlnSleep(2000, "The guard grabs your arm and leads you to the front gate.");
+		Printers.printlnSleep(3000, "Once you're there, the guard grunts and points to a new passageway all the way on the other side of the village.");
+		Printers.printlnSleep(1500, "The guard moves to block you from reentering the village.");
+		Printers.printlnSleep(2000, "You flick your phone flashlight back on and see a narrow passageway.");
+		fall();		
 	}
 	
-	public static void id() {
-		Printers.printlnSleep(2000, "You find your id and hand it to the guard.");
-		Printers.printlnSleep(2000, "The guard takes a look at your id and hands it back to you.");
-		Printers.printlnSleep(2000, "\"Not sure where this is from, but it looks fine.\"");
-		townSquare();
-	}
-	
-	public static void noID() {
-		Printers.printlnSleep(2500, "\"I must've lost my id when I fell down here, I'm just trying to get back home\"");
-		Printers.printlnSleep(1000, "The guard scratches his chin and looks around.");
-		Printers.printlnSleep(2000, "\"Alright, I'll let you in but tell NOBODY you don't have a passport. Got it?\"");
-		Printers.printlnSleep(500, "You quickly nod.");
-		townSquare();
-	}
-	
-	public static void townSquare() {
-		Printers.printlnSleep(3000, "He signals you through and you enter the town. You see kids playind and adults watching, all dressed "
-				+ "in colonial garb.");
-		Printers.printlnSleep(2000, "There are a couple shops and a little town hall in the center of the town.");
-		Printers.printlnSleep(3000, "Your entrance doesn't go unnoticed for long. People slowly stop talking at start looking at you. After just a few "
-				+ "minutes, the town goes completely silent.");
-		
-		switch(Printers.optionDialog("You feel obligated to say something.", 
-		new String[] {"\"Does anyone know a way to get out of this cave?\"","\"Just passing through.\""})) {
-		case 0: followChoice(); 
-		break;
-		case 1: Printers.printlnSleep(2500, "A look of disappointment washes over the townsfolk.");
-		Caves.begin(); 
-		break;
-		
-		}
-	}
-	
-	public static void followChoice() {
-		Printers.printlnSleep(2000, "An older man emerges from the crowd and asks you to follow him.");
-		if(isHostile)
-			Printers.printlnSleep(2000, "You feel an immense pressure to agree.");
-		if(Printers.optionDialog("Do you want to follow him?", new String[] {"Yeah, he seems nice.", "No, I don't trust him."}) == 0) {
-			if(isHostile)
-				hostileFollow();
-			else
-				passiveFollow();		
-		} else {
-			if(isHostile)
-				hostileNoFollow();
-			else {
-				Printers.printlnSleep(2500, "A look of disappointment washes over the townsfolk.");
-				Printers.printlnSleep(2000, "\"So be it.\" The old man says with a tinge of sorrow in his voice.");
-				Printers.printlnSleep(2000, "The old man turns away from you and gestures towards the guard");
-				Caves.begin();	
-			}
-		}
-		
-	}
-	
-	public static void passiveFollow() {
-		Printers.printlnSleep(2000, "The old man's straight face turns to a hearty smile");
-		Printers.printlnSleep(2000, "\"Follow me into the town hall, young " + Main.player.genderString + ".");
-		Printers.printlnSleep(3000, "The old man sits you down in a room full of photographs, documents, and all sorts of artifacts.");
-		Printers.printlnSleep(2000, "He says: \"I'll lead you up to the surface, but I need you to do something for me as well.\"");
-		Printers.printlnSleep(1000, "\"Every exit to this cave is sealed off.\"");
-		Printers.printlnSleep(2000, "\"However, they open whenever a tour comes through.\"");
-		Printers.printlnSleep(3000, "\"I need you to sneak into a tour and when you leave, go tell someone important that we're down here.\"");
-		Printers.printlnSleep(2000, "\"You see, we've been down here for generations.\"");
-		Printers.printlnSleep(3000, "\"Our town had a religious split. Most of the citizens followed were in one sect, while we were in the other\"");
-		Printers.printlnSleep(3000, "\"Finally, after years of abuse and harassment, they drove us into these caves and sealed up all the exits.\"");
-		Printers.printlnSleep(2000, "\"It's been well over a hundred years, but the townspeople on the surface have decided to keep us down here");
-	}
-	
-	public static void hostileFollow() {
-		Printers.printlnSleep(2000, "The old man's straight face turns to a hearty smile");
-		Printers.printlnSleep(2000, "\"Follow me into the town hall, young " + Main.player.genderString + ".");
-		Printers.printlnSleep(3000, "The old man sits you down in a room full of photographs, documents, and all sorts of artifacts.");
-		Printers.printlnSleep(2000, "\"You see, we've been down here for generations.\"");
-		Printers.printlnSleep(3000, "\"Our town had a religious split. Most of the citizens were in one sect, while we were in the other\"");
-		Printers.printlnSleep(3000, "\"Finally, after years of abuse and harassment, they drove us into these caves and sealed up all the exits.\"");
-		Printers.printlnSleep(2000, "\"It's been well over a hundred years, but the townspeople on the surface have decided to keep us down here for whatever reason.\"");
-		Printers.printlnSleep(3000, "\"Every decade or so, someone like you comes down here. Someone with knowledge of the outside world,");
-		Printers.printlnSleep(2000, "Someone who might be able to help us get out of here.\"");
-		Printers.printSleep(500, "\"Every ");
-		Printers.printSleep(500, "single ");
-		Printers.printSleep(500, "person ");
-		Printers.printlnSleep(2000, "who comes down here ends up breaking their promise.\"");
-		Printers.printlnSleep(3000, "\"We'll tell you how to get back to the surface, but you need to promise that you'll do whatever you can to get us out of here.\"");
-		switch(Printers.optionDialog("What do you want to say?", new String[] {"\"I promise.","\"I can't promise anything."})) {
+	public static void fall() {
+		Printers.printlnSleep(3000, "You take on step in and immediately trip and start tumbling down a steep slope.");
+		Printers.printlnSleep(3000, "Suddenly, you fly headfirst out of the tunnel and into a huge pool of water. ");
+		Printers.printlnSleep(1500, "You swim to the surface and gasp for air.");
+		Printers.printlnSleep(3500, "You poke your head out of the water and come face to face with a party of around 10 people,"
+				+ "\nDressed in cargo shorts with cameras around their necks and fanny packs around their waists.");
+		Printers.printlnSleep(2000, "They're all looking at you starry-eyed.");
+		switch(Printers.optionDialog("What would you like to say?", new String[] {"Are you guys from the surface?", "I think I got seperated from the group."})) {
 		case 0:
-		case 1: howAreYouThisStupid();
+			Printers.printlnSleep(2000, "The group's confusion turns to terror as they bolt away, shutting off the lights behind them.");
+			Printers.printlnSleep(3000, "You try to give chase but they're out of sight before you can get out of the water.");
+			if(!feelAround())
+				starve();
+			Printers.printlnSleep(2000, "Suddenly, your finger flicks a switch on.");
+			Printers.printlnSleep(2000, "The row of plastic lights turn back on.");
+			switch(Printers.optionDialog("Where do you want to go?", new String[] {"I'll go the same way that group went.", "I'll go the other way."})){
+			case 0: 
+				Printers.ellipsis(3);
+				Printers.printlnSleep(2000, "After an eternity of walking, you reach a metal door.");
+				Printers.printlnSleep(3000, "You bang on the door, but nobody answers.");
+				Printers.printlnSleep(2000, "You bang even harder and yell, until you finally get a response:");
+				Printers.printlnSleep(1500, "\"Go back to your town, we don't want you here!\"");
+				Printers.printlnSleep(2000, "You try to say something, but they just repeating that phrase over and over.");
+				Printers.printlnSleep(1000, "They turn off the lights again.");
+				Printers.printlnSleep(2000, "You feel around your pockets for your food but all you can find is your phone, destroyed by the fall.");
+				starve();
+				break;
+			case 1:
+				Printers.printlnSleep(2000, "After an eternity of walking, you reach a metal door.");
+				Printers.printlnSleep(3000, "You bang on the door, but nobody answers.");
+				Printers.printlnSleep(2000, "After a short wait, you push the door open yourself.");
+				Printers.printlnSleep(2000, "The sun is overwhelming, although in a good way.");
+				Printers.printlnSleep(2000, "You made it.");
+				Player.end();
+				break;
+			}
+		case 1:
+			Printers.printlnSleep(3000, "\"You could've gotten lost, or even died!\" Shouts the tour guide. She apologizes to the other guests and"
+					+ " shouts for you to come rejoin the group.");
+			Printers.printlnSleep(2000, "You follow the group all the way to the exit, and you finally reach the exit door.");
+			Printers.printlnSleep(2000, "The tour guide pushes the door open.");
+			Printers.printlnSleep(2000, "The sun is overwhelming, although in a good way.");
+			Printers.printlnSleep(2000, "You made it.");
+			Player.end();
+			break;
+			
 		}
+		
 	}
 	
-	public static void hostileNoFollow() {
-		Printers.printSleep(2500, "The cautious curiosity the townsfolk had towards you only a moment ago was all gone.");
-		Printers.printlnSleep(1000," Now, all that's left is hostility");
-		Printers.printlnSleep(2500,"You hear the guard move to block off the entrance and the old man retreats into the crowd.");
-		Printers.printlnSleep(1500, "Before you know it, there's a crowd of people descending on you.");
+	public static boolean feelAround() {
+	int numAttempts = 1;
+		
+		while(!Roll.silentRoll(1, 20) || numAttempts > 11) {
+			Printers.printSleep(2000,"You've been looking for " + numAttempts * 10 + " minutes, but you haven't found anything yet.");
+			numAttempts++;
+		}
+		
+		if (numAttempts > 11) {
+			Printers.printlnSleep(1000, "You give up.");
+			return false;
+		}
+		return true;
+	}
+	
+	public static void starve() {
+		Printers.printlnSleep(3000, "Alone, quiet, and in complete darkness, you realize it's your time to go.");
+		Printers.printlnSleep(4000, "With no food, a destroyed phone, and no idea where you are, the chances that someone else comes along to help "
+				+ "you are pretty slim.");
 		Player.death();
 	}
-	
-	public static void howAreYouThisStupid() {
-		Printers.printlnSleep(2000, "\"Huh. I'll give you this, you're the first person to actually tell the truth.\"");
-		Printers.printlnSleep(500, "Still...");
-		Printers.printlnSleep(0, null);
-	}
-	
 }
+
